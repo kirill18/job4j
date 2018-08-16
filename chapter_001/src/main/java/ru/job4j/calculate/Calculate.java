3f39a268563bbccd04525a6cac094fc903fcec52
kirill18 <kirill_lityuchii@mail.ru>
2018-08-16 23:39:39 +0400

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,16 @@
 package ru.job4j.calculate;
-
+/**
+	*Класс. Должен совпадать с именем файла.
+	*/
 public class Calculate {
-	
+	/**
+	*main - сновной метод для запуска приложения 
+	*объявлен как public, static, void
+	*метод println выводит на консоль текст Hello World
+	*параметр ln переносит курсор на следующую строку
+	*@param Strings[] args - массив из строк с аргументами, которые ей передали
+	*@return Выводит на консоль текст Hello World
+	*/
 	public static void main(String[] args) {
 		System.out.println ("Hello World");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Kirill Lityuchii (Kirill_lityuchii@mail.ru)
+* @version 1.0
+* @since 1.0
+*/
+package ru.job4j.calculate;
