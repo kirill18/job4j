98f603f9cd4996c1ad9d94d39d08d52efa081642
kirill18 <kirill_lityuchii@mail.ru>
2018-08-26 23:01:26 +0400

    delete Max.java

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -3,11 +3,6 @@ package ru.job4j.max;
 public class Max {
 
     public int max(int first, int second) {
-<<<<<<< HEAD
         return first - second > 0 ? first : second;
-=======
-        int value = first-second>0 ? first : second;
-        return value;
->>>>>>> origin/master
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -17,3 +17,4 @@ public class PointTest {
         assertThat(result, closeTo(expected, 0.1));
     }
 }
+
