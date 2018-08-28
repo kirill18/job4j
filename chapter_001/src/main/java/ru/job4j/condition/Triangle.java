0aa18fc2cb99b73d05305a4a158386eb6649b6c4
kirill18 <kirill_lityuchii@mail.ru>
2018-08-28 23:12:43 +0400

    4.3. Вычисление площади треугольника [#9461]
    Signed-off-by: kirill18 <kirill_lityuchii@mail.ru>

---
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -0,0 +1,33 @@
+package ru.job4j.condition;
+
+public class Triangle {
+    private Point a;
+    private Point b;
+    private Point c;
+
+    public Triangle(Point a, Point b, Point c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double period(double ab, double ac, double bc) {
+        return (ab + ac + bc) / 2;
+    }
+    public double area() {
+        double rsl = -1; // мы устанавливаем значение -1, так как может быть что треугольника нет. Это значение говорит о том. что треугольника нет.
+        double ab = this.a.distanceTo(this.b);
+        double ac = this.a.distanceTo(this.c);
+        double bc = this.b.distanceTo(this.c);
+        double p = this.period(ab, ac, bc);
+        if (this.exist(ab, ac, bc)) {
+            rsl = Math.sqrt(p * (p - ab) * (p - bc) * (p - ac));
+        }
+            return rsl;
+        }
+        private boolean exist(double ab, double ac, double bc) {
+        if (ab - ac > bc && (bc - ac > ab && ab - bc > ac)) {
+    }
+            return true;
+    }
+}
