45d9d0803e3710df569a10fb2cdf5f70b99025b4
kirill18 <kirill_lityuchii@mail.ru>
2018-09-09 19:15:21 +0400

    6.6. Двухмерный массив. Таблица умножения. [#33491]
    Signed-off-by: kirill18 <kirill_lityuchii@mail.ru>

---
+++ chapter_001/src/main/java/ru/job4j/matrix/Matrix.java
@@ -0,0 +1,27 @@
+package ru.job4j.matrix;
+
+/**
+ * @author Kirill Lityuchii (kirill_lityuchii@mail.ru)
+ * @version 1.0
+ * @since 09.09.2018
+ */
+
+public class Matrix {
+
+    /**
+     * Метод поочередно перемножает все элементы матрицы
+     *
+     * @param size размер матрицы.
+     * @return возвращает двумерный массив с перемноженными элементами
+     */
+
+    public int[][] multiple(int size) {
+        int[][] table = new int[size][size];
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table.length; j++) {
+                table[i][j] = (i+1) * (j+1);
+            }
+        }
+        return table;
+    }
+}
