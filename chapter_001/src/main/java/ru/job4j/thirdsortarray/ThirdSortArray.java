bfa308c344e9b276438fb36337c2639fec1a0428
kirill18 <kirill_lityuchii@mail.ru>
2018-09-16 18:47:39 +0400

    7.1 Запись значений из двух сортированных массивов в третий массив с последующей его сортировкой
    Signed-off-by: kirill18 <kirill_lityuchii@mail.ru>

--- chapter_001/src/main/java/ru/job4j/thirdsortarray/ThirdSortArray.java
+++ chapter_001/src/main/java/ru/job4j/thirdsortarray/ThirdSortArray.java
@@ -1,37 +1,35 @@
 package ru.job4j.thirdsortarray;
 
+/**
+ * @author Kirill Lityuchii (kirill_lityuchii@mail.ru)
+ * @version 1.2
+ * @since 16.09.2018
+ */
+
 public class ThirdSortArray {
 
-    public int[] PlusSortArrays(int[] array1, int[] array2) {
+    /**
+     * Метод помещает значения из двух сортированных массивов в терий, с последующей его сортировкой.
+     *
+     * @param array1,array2 суммируемые мвассивы.
+     * @return возвращает третий отсортированный массив
+     */
+
+    public int[] plusSortArrays(int[] array1, int[] array2) {
         int[] SumArray = new int[array1.length + array2.length];
-         /*Индексы эллементов суммируемых массивов.Образуют собой буфер.
-        В результате сравнения индексов i и j в буфере будет храниться большее значение
-        для последующего сравнения с следующим эллементом одного из суммируемых массивов.*/
-        int i = 0, j = 0;
+                int i = 0, j = 0;
         for (int index = 0; index < SumArray.length; index++) {
-        /*Проверяет чтобы эллементы суммируемыех массивов не были превышены.
-         Если индексы i или j превысят колличество эллементов в суммируемых массивах, то
-           значит осталься последний наибольший эллемент.*/
-            if (i > array1.length - 1) {
-                int a = array2[j];
-                SumArray[index] = a;
-                j++;
-            } else if (j > array2.length - 1) {
-                int a = array1[i];
-                SumArray[index] = a;
-                i++;
-                /*Проверяет наибольший эллемент из суммируемых массивов.
-                 результате сравнения наименьшее значение записывается в результирующий массив.
-                 Переходит к следующему эллементу массива в котором было минимальное значение
-                 для последующего его сравнения с максимальным эллементом по результатам предыдущей операции.*/
-            } else if (array1[i] < array2[j]) {
-                int a = array1[i];
-                SumArray[index] = a;
-                i++;
-            } else {
-                int b = array2[j];
-                SumArray[index] = b;
-                j++;
+                if (i > array1.length - 1) {
+                SumArray[index] =array2[j++];
+            }
+            else if (j > array2.length - 1) {
+                SumArray[index] = array1[i++];
+                }
+                else if (array1[i] < array2[j]) {
+                SumArray[index] = array1[i++];
+            }
+            else {
+                SumArray[index] = array2[j++];
             }
         }
         return SumArray;
--- chapter_001/src/test/java/ru/job4j/thirdsortarray/ThirdSortArrayTest.java
+++ chapter_001/src/test/java/ru/job4j/thirdsortarray/ThirdSortArrayTest.java
@@ -7,13 +7,39 @@ import static org.junit.Assert.assertThat;
 
 public class ThirdSortArrayTest {
     @Test
-    public void WhenThirdArrayThisSumFirstAndSeconaArrays () {
+    public void whenThirdArrayThisSumFirstAndSeconaArrays() {
         ThirdSortArray thirdarray = new ThirdSortArray();
         int[] input1 = new int[] {1, 3, 5, 7, 9};
         int[] input2 = new int[] {0, 2, 4, 6};
         int[] expect = {0, 1, 2, 3, 4, 5, 6, 7, 9};
-        int[] result = thirdarray.PlusSortArrays(input1, input2);
+        int[] result = thirdarray.plusSortArrays(input1, input2);
         assertThat(result, is(expect));
     }
-
+    @Test
+    public void whenThirdArrayThisSumNullFirstAndSeconaArrays() {
+        ThirdSortArray thirdarray = new ThirdSortArray();
+        int[] input1 = new int[] {};
+        int[] input2 = new int[] {0, 2, 4, 6};
+        int[] expect = {0, 2, 4, 6};
+        int[] result = thirdarray.plusSortArrays(input1, input2);
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenThirdArrayThisSumFirstAndNullSeconaArrays() {
+        ThirdSortArray thirdarray = new ThirdSortArray();
+        int[] input1 = new int[] {1, 3, 5, 7, 9};
+        int[] input2 = new int[] {};
+        int[] expect = {1, 3, 5, 7, 9};
+        int[] result = thirdarray.plusSortArrays(input1, input2);
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenThirdArrayThisSumFirstAndSeconaArrays1() {
+        ThirdSortArray thirdarray = new ThirdSortArray();
+        int[] input1 = new int[] {1, 3, 5};
+        int[] input2 = new int[] {0, 2, 4, 6};
+        int[] expect = {0, 1, 2, 3, 4, 5, 6};
+        int[] result = thirdarray.plusSortArrays(input1, input2);
+        assertThat(result, is(expect));
+    }
 }
