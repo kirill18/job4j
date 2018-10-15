82752502001d642c4d74285d4cac0f52def840b1
kirill18 <kirill_lityuchii@mail.ru>
2018-10-15 21:57:27 +0400

    7. Крестики нолики (версия 3)
    Signed-off-by: kirill18 <kirill_lityuchii@mail.ru>

--- chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
+++ chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
@@ -14,13 +14,7 @@ public class Logic3T {
         for (int i = 0; i < table.length; i++) {
             result = true;
             for (int j = 0; j < table.length - 1; j++) {
-                if (!(table[j][i].hasMarkX() && table[j + 1][i].hasMarkX())
-                        &&
-                        !(table[i][j].hasMarkX() && table[i][j + 1].hasMarkX())
-                        &&
-                        !(table[j][j].hasMarkX() && table[j + 1][j + 1].hasMarkX())
-                        &&
-                        !(table[j][table.length - j - 1].hasMarkX() && table[table.length - 1][0].hasMarkX())) {
+                if (!(table[j][i].hasMarkX() && table[j + 1][i].hasMarkX())) {
                     result = false;
                     break;
                 }
@@ -29,32 +23,110 @@ public class Logic3T {
                 break;
             }
         }
+        if (!result) {
+            for (int i = 0; i < table.length; i++) {
+                result = true;
+                for (int j = 0; j < table.length - 1; j++) {
+                    if (!(table[i][j].hasMarkX() && table[i][j + 1].hasMarkX())) {
+                        result = false;
+                        break;
+                    }
+                }
+                if (result) {
+                    break;
+                }
+            }
+        }
+        if (!result) {
+            for (int i = 0; i < table.length; i++) {
+                result = true;
+                for (int j = 0; j < table.length - 1; j++) {
+                    if (!(table[j][table.length - j - 1].hasMarkX() && table[table.length - 1][0].hasMarkX())) {
+                        result = false;
+                        break;
+                    }
+                }
+                if (result) {
+                    break;
+                }
+            }
+        }
+        if (!result) {
+            for (int i = 0; i < table.length; i++) {
+                result = true;
+                for (int j = 0; j < table.length - 1; j++) {
+                    if (!(table[j][j].hasMarkX() && table[j + 1][j + 1].hasMarkX())) {
+                        result = false;
+                        break;
+                    }
+                }
+                if (result) {
+                    break;
+                }
+            }
+        }
         return result;
     }
 
     public boolean isWinnerO() {
-            boolean result = false;
+        boolean result = true;
+        for (int i = 0; i < table.length; i++) {
+            result = true;
+            for (int j = 0; j < table.length - 1; j++) {
+                if (!(table[j][i].hasMarkO() && table[j + 1][i].hasMarkO())) {
+                    result = false;
+                    break;
+                }
+            }
+            if (result) {
+                break;
+            }
+        }
+        if (!result) {
             for (int i = 0; i < table.length; i++) {
+                result = true;
                 for (int j = 0; j < table.length - 1; j++) {
-                        if (table[j][i].hasMarkO() && table[j + 1][i].hasMarkO()) {
-                            result = true;
-                        }
-                        else if (table[i][j].hasMarkO() && table[i][j + 1].hasMarkO()) {
-                            result = true;
-                        }
-                        else if (table[j][j].hasMarkO() && table[j + 1][j + 1].hasMarkO()) {
-                            result = true;
-                        }
-                        else if (table[j][table.length - j - 1].hasMarkO() && table[table.length - 1][0].hasMarkO()) {
-                            result = true;
-                        }
-                    }
-                    if (result) {
+                    if (!(table[i][j].hasMarkO() && table[i][j + 1].hasMarkO())) {
+                        result = false;
                         break;
                     }
+                }
+                if (result) {
+                    break;
+                }
             }
-            return result;
         }
+        if (!result) {
+        for (int i = 0; i < table.length; i++) {
+            result = true;
+            for (int j = 0; j < table.length - 1; j++) {
+                if (!(table[j][table.length - j - 1].hasMarkO() && table[table.length - 1][0].hasMarkO())) {
+                    result = false;
+                    break;
+                }
+            }
+            if (result) {
+                break;
+            }
+        }
+    }
+        if (!result) {
+            for (int i = 0; i < table.length; i++) {
+                result = true;
+                for (int j = 0; j < table.length - 1; j++) {
+                    if (!(table[j][j].hasMarkO() && table[j + 1][j + 1].hasMarkO())) {
+                        result = false;
+                        break;
+                    }
+                }
+                if (result) {
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
 
 
         public boolean hasGap() {
