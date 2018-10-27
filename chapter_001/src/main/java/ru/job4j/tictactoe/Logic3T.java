7a5c33b757671d11fbffec22e9624cc0fb5c4d5f
kirill18 <kirill_lityuchii@mail.ru>
2018-10-28 00:17:03 +0400

    7. Крестики нолики (Версия 5)
    Signed-off-by: kirill18 <kirill_lityuchii@mail.ru>

--- chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
+++ chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
@@ -11,52 +11,58 @@ public class Logic3T {
     }
 
     public boolean isWinnerX() {
-        return this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 0)
-                ||
-                this.fillBy(Figure3T::hasMarkX, 0, 0, 0, 1)
-                ||
-                this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 1)
-                ||
-                this.fillBy(Figure3T::hasMarkX, this.table.length - 1, 0, -1, 1)
-                ||
-                this.fillBy(Figure3T::hasMarkX, this.table.length - 2, 0, 0, 1)
-                ||
-                this.fillBy(Figure3T::hasMarkX, this.table.length - 1, 0, 0, 1)
-                ||
-                this.fillBy(Figure3T::hasMarkX, 0, this.table.length - 1, 1, 0)
-                ||
-                this.fillBy(Figure3T::hasMarkX, 0, this.table.length - 2, 1, 0);
+    boolean result = false;
+        for (int i = 0; i < table.length; i++) {
+            if (fillBy(Figure3T::hasMarkX, 0, i, 1, 0)) {
+                result = true;
+                break;
+            }
+        }
+            for (int j = 0; j < table.length; j++) {
+                if (fillBy(Figure3T::hasMarkX, j, 0, 0, 1)) {
+                    result = true;
+                    break;
+                }
+            }
+            if (fillBy(Figure3T::hasMarkX, 0, 0, 1, 1)
+                || fillBy(Figure3T::hasMarkX, this.table.length - 1, 0, -1, 1)) {
+                result = true;
+            }
+          return result;
     }
 
 
     public boolean isWinnerO() {
-        return this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 0)
-                ||
-                this.fillBy(Figure3T::hasMarkO, 0, 0, 0, 1)
-                ||
-                this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 1)
-                ||
-                this.fillBy(Figure3T::hasMarkO, this.table.length - 1, 0, -1, 1)
-                ||
-                this.fillBy(Figure3T::hasMarkO, this.table.length - 2, 0, 0, 1)
-                ||
-                this.fillBy(Figure3T::hasMarkO, this.table.length - 1, 0, 0, 1)
-                ||
-                this.fillBy(Figure3T::hasMarkO, 0, this.table.length - 1, 1, 0)
-                ||
-                this.fillBy(Figure3T::hasMarkO, 0, this.table.length - 2, 1, 0);
+        boolean result = false;
+        for (int i = 0; i < table.length; i++) {
+            if (fillBy(Figure3T::hasMarkO, 0, i, 1, 0)) {
+                result = true;
+                break;
+            }
+        }
+        for (int j = 0; j < table.length; j++) {
+            if (fillBy(Figure3T::hasMarkO, j, 0, 0, 1)) {
+                result = true;
+                break;
+            }
+        }
+        if (fillBy(Figure3T::hasMarkO, 0, 0, 1, 1)
+                || fillBy(Figure3T::hasMarkO, this.table.length - 1, 0, -1, 1)) {
+            result = true;
+        }
+        return result;
     }
 
     public boolean fillBy(Predicate<Figure3T> predicate, int startX, int startY, int deltaX, int deltaY) {
         boolean result = true;
-        for (int index = 0; index != this.table.length; index++) {
-            Figure3T cell = this.table[startX][startY];
-            startX += deltaX;
-            startY += deltaY;
-            if (!predicate.test(cell)) {
-                result = false;
-                break;
-            }
+            for (int j = 0; j != this.table.length; j++) {
+                Figure3T cell = this.table[startX][startY];
+                startX += deltaX;
+                startY += deltaY;
+                if (!predicate.test(cell)) {
+                    result = false;
+                    break;
+                }
         }
         return result;
     }
