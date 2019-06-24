87e2ceb6524c4ea683db9a95560dca4a2056f3bb
kirill18 <kirill_lityuchii@mail.ru>
2019-06-24 23:31:45 +0300

    2. Рефакторинг - Площадь треугольника

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -12,15 +12,14 @@ public class Triangle {
     }
 
     public double period(double ab, double ac, double bc) {
-
         return (ab + ac + bc) / 2;
     }
 
     public double area() {
         double rsl = -1; // мы устанавливаем значение -1, так как может быть что треугольника нет. Это значение говорит о том. что треугольника нет.
-        double ab = this.a.distanceTo(this.b);
-        double ac = this.a.distanceTo(this.c);
-        double bc = this.b.distanceTo(this.c);
+        double ab = a.distance(b);
+        double ac = a.distance(c);
+        double bc = b.distance(c);
         double p = this.period(ab, ac, bc);
         if (this.exist(ab, ac, bc)) {
             rsl = Math.sqrt(p * (p - ab) * (p - bc) * (p - ac));
