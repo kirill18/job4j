e39796ac1f856e3ce5aa602b1f4bace0683dea79
kirill18 <kirill_lityuchii@mail.ru>
2019-06-24 23:20:50 +0300

    1. Рефакторинг - Расстояние между точками

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -1,17 +1,23 @@
 package ru.job4j.condition;
 
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
 public class Point {
+
     private int x;
     private int y;
 
-    public  Point(int x, int y) {
-        this.x = x;
-        this.y = y;
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
     }
-    public double distanceTo(Point that) {
-        return Math.sqrt(
-                Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2)
-        );
 
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
+
+    public void info() {
+        System.out.println(String.format("Point[%s, %s]", this.x, this.y));
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -2,19 +2,35 @@ package ru.job4j.condition;
 
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.number.IsCloseTo.closeTo;
-
 import static org.junit.Assert.assertThat;
 
 public class PointTest {
+    @Test
+    public void whenZeroAndTenThenTen() {
+        Point first = new Point(6, 8);
+        Point second = new Point(2, 4);
+        double result = first.distance(second);
+        first.info();
+        second.info();
+        System.out.println(String.format("Result is %s", result));
+        double expected = 5.6;
+        assertThat(result, closeTo(expected, 5.6));
+    }
 
     @Test
-    public void aDistanceToB() {
-        Point a = new Point(3, 6);
-        Point b = new Point(4, 9);
-        double result = a.distanceTo(b);
-        double expected = 3.1;
-        assertThat(result, closeTo(expected, 0.1));
+    public void whenCheckItself() {
+        Point point = new Point(4, 8);
+        double result = point.distance(point);
+        assertThat(result, is(0D));
+    }
+
+    @Test
+    public void whenShowInfo() {
+        Point first = new Point(1, 1);
+        first.info();
+        Point second = new Point(2, 2);
+        second.info();
     }
 }
-
